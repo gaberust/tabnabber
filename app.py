da55ff63e67cc9b21e86ae884ee50be638eb7afc
Gabe Rust <gabe@rekt.run>
2023-07-12 18:21:22 -0400

    Initial Commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', context={'dest': request.args.get('dest')})
+    return render_template('index.html', dest=request.args.get('dest'))
 
 
 if __name__ == '__main__':
